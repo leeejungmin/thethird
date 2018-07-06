d8bb46d44db39b2827c0d368376ec2e5ab4c5b01
jungminlee <dlwjdals6424@naver.com>
2018-07-06 08:36:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dlwjd/thethird/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.dlwjd.thethird;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Integer num = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gotoSecondActivity(View view){
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void changeColor(View view){
+
+        textView = (TextView)(findViewById(R.id.textViewHello));
+
+        if (num==0){
+
+            textView.setTextColor(Color.parseColor("#FF0000"));
+            textView.setText("This is RED");
+            num=1;
+
+        }else{
+
+            textView.setTextColor(Color.parseColor("#0000FF"));
+            textView.setText("This is BLUE");
+            num=0;
+
+        }
+    }
+}
